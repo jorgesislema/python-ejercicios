895c7b6d962b194b86165b673acc8f25ce81048b
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-05-24 18:27:15 -0500

    Create 06_while_basico.py

---
+++ 02-Control-de-Flujo/06_while_basico.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 6: Bucle while básico
+
+Objetivo: Comprender el uso del bucle while.
+
+Instrucciones:
+- Escribe un programa que muestre los números del 1 al 10 usando while.
+"""
+
+contador = 1
+while contador <= 10:
+    print(contador)
+    contador += 1
