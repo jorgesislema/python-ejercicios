f41902e55a9bbb9ad97dc778c30fe5bf843b262d
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-05-24 18:27:10 -0500

    Create 05_operador_ternario.py

---
+++ 02-Control-de-Flujo/05_operador_ternario.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 5: Operador ternario
+
+Objetivo: Usar el operador ternario para simplificar condiciones.
+
+Instrucciones:
+- Escribe un programa que pida un número y muestre si es par o impar usando una sola línea con operador ternario.
+"""
+
+n = int(input("Introduce un número: "))
+print("Par" if n % 2 == 0 else "Impar")
