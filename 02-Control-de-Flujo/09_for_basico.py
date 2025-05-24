ad783560a7b404c4c7eb116b0b0c6581f19db66c
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-05-24 18:27:28 -0500

    Create 09_for_basico.py

---
+++ 02-Control-de-Flujo/09_for_basico.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 9: Bucle for básico
+
+Objetivo: Comprender el uso del bucle for.
+
+Instrucciones:
+- Escribe un programa que muestre los elementos de una lista de frutas usando for.
+"""
+
+frutas = ["manzana", "banana", "naranja", "pera"]
+for fruta in frutas:
+    print(fruta)
