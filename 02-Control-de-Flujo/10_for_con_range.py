880656d385399b041a4e47a409282d6b34aaec3a
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-05-24 18:27:32 -0500

    Create 10_for_con_range.py

---
+++ 02-Control-de-Flujo/10_for_con_range.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 10: for con range
+
+Objetivo: Usar range en bucles for.
+
+Instrucciones:
+- Escribe un programa que muestre los números pares del 2 al 20 usando for y range.
+"""
+
+for i in range(2, 21, 2):
+    print(i)
