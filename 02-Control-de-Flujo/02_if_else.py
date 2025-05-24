e313cdb3c6e9b5257e2092ed1c607ba4a1eaf46f
jorgesislema <79588110+jorgesislema@users.noreply.github.com>
2025-05-24 18:26:55 -0500

    Create 02_if_else.py

---
+++ 02-Control-de-Flujo/02_if_else.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio 2: if-else
+
+Objetivo: Practicar el uso de la estructura if-else.
+
+Instrucciones:
+- Escribe un programa que pida la edad al usuario y muestre si es mayor o menor de edad.
+"""
+
+edad = int(input("Introduce tu edad: "))
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
